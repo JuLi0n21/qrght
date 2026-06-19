076fa06fba612ac2a94e7f19174ca0f2f158cd75
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2026-06-19 15:46:42 +0200

    create qrcode generator

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev environment for Horsa";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go_1_26
+
+          nodejs_25
+          nodePackages.npm
+        ];
+      };
+    };
+}
